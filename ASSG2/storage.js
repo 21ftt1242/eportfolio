bd4f86c6bf1d2a8b4f45353feffe5bd5bfd25ff9
Muaz Harris <21FTT1242@student.pb.edu.bn>
2022-05-20 14:45:58 +0800

    aaa

--- ASSG2/storage.js
+++ ASSG2/storage.js
@@ -1,9 +1,10 @@
  //localStorage
 
  localStorage.setItem('story-1a', 'Once when I was six years old I saw a magnificent picture in a book, called True Stories from Nature, about the primeval forest. It was a picture of a boa constrictor in the act of swallowing an animal. Here is a copy of the drawing. In the book it said: "Boa constrictors swallow their prey whole, without chewing it. After that they are not able to move, and they sleep through the six months that they need for digestion." I pondered deeply, then, over the adventures of the jungle. And after some work with a colored pencil I succeeded in making my first drawing. My Drawing Number One. It looked like this: <br> <img src="images/story1-img2.png">');
- localStorage.setItem('story-1b', 'I showed my masterpiece to the grown-ups, and asked them whether the drawing frightened them. But they answered: "Frighten? Why should any one be frightened by a hat?" My drawing was not a picture of a hat. It was a picture of a boa constrictor digesting an elephant. But since the grown-ups were not able to understand it, I made another drawing: I drew the inside of the boa constrictor, so that the grown-ups could see it clearly. They always need to have things explained. My Drawing Number Two looked like this:');
+ localStorage.setItem('story-1b', 'I showed my masterpiece to the grown-ups, and asked them whether the drawing frightened them. But they answered: "Frighten? Why should any one be frightened by a hat?" My drawing was not a picture of a hat. It was a picture of a boa constrictor digesting an elephant. But since the grown-ups were not able to understand it, I made another drawing: I drew the inside of the boa constrictor, so that the grown-ups could see it clearly. They always need to have things explained. My Drawing Number Two looked like this: <br> <img src="images/story1-img2.png">');
 
  document.getElementById("content1").innerHTML = localStorage.getItem("story-1a");
+document.getElementById("content2").innerHTML = localStorage.getItem("story-1b");
 
 
   // Declare variables
@@ -15,6 +16,7 @@ var synth = window.speechSynthesis;
 
 // Get Story text
 var text = document.getElementById("content1").innerText;
+var text2 = document.getElementById("content2").innerText;
 
 function nextPage() {
 document.getElementById("story1").innerHTML = localStorage.getItem("story-1b");
@@ -55,7 +57,7 @@ if(synth.onvoiceschanged !== undefined) {
 
 // ----------Play Button----------
 
-function playText(content1) {
+function playText() {
   let selectedVoice = voiceSelect.selectedOptions[0].getAttribute('data-name');
 
   voices.forEach((voice) => {
