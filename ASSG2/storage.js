5afc5a5bf8443c5433ca8625d28a4500912f3e8b
Muaz Harris <21FTT1242@student.pb.edu.bn>
2022-05-21 15:55:29 +0800

    nearly done

--- ASSG2/storage.js
+++ ASSG2/storage.js
@@ -1,11 +1,11 @@
  //localStorage
 
- localStorage.setItem('story-1a', 'Once when I was six years old I saw a magnificent picture in a book, called True Stories from Nature, about the primeval forest. It was a picture of a boa constrictor in the act of swallowing an animal. Here is a copy of the drawing. In the book it said: "Boa constrictors swallow their prey whole, without chewing it. After that they are not able to move, and they sleep through the six months that they need for digestion." I pondered deeply, then, over the adventures of the jungle. And after some work with a colored pencil I succeeded in making my first drawing. My Drawing Number One. It looked like this: <img src="images/story1-img2.png">');
- localStorage.setItem('story-1b', 'I showed my masterpiece to the grown-ups, and asked them whether the drawing frightened them. But they answered: "Frighten? Why should any one be frightened by a hat?" My drawing was not a picture of a hat. It was a picture of a boa constrictor digesting an elephant. But since the grown-ups were not able to understand it, I made another drawing: I drew the inside of the boa constrictor, so that the grown-ups could see it clearly. They always need to have things explained. My Drawing Number Two looked like this: <br> <img src="images/story1-img2.png">');
+ localStorage.setItem('story-1a', '<img src="images/story1-img2.png"> Once when I was six years old I saw a magnificent picture in a book, called True Stories from Nature, about the primeval forest. It was a picture of a boa constrictor in the act of swallowing an animal. Here is a copy of the drawing. In the book it said: "Boa constrictors swallow their prey whole, without chewing it. After that they are not able to move, and they sleep through the six months that they need for digestion." I pondered deeply, then, over the adventures of the jungle. And after some work with a colored pencil I succeeded in making my first drawing. My Drawing Number One. It looked like this: <img src="images/story1-img3.png">');
+ localStorage.setItem('story-1b', 'I showed my masterpiece to the grown-ups, and asked them whether the drawing frightened them. But they answered: "Frighten? Why should any one be frightened by a hat?" My drawing was not a picture of a hat. It was a picture of a boa constrictor digesting an elephant. But since the grown-ups were not able to understand it, I made another drawing: I drew the inside of the boa constrictor, so that the grown-ups could see it clearly. They always need to have things explained. My Drawing Number Two looked like this: <br> <img src="images/story1-img4.png">');
  localStorage.setItem('story-1c', 'The grown-ups\' response, this time, was to advise me to lay aside my drawings of boa constrictors, whether from the inside or the outside, and devote myself instead to geography, history, arithmetic and grammar. That is why, at the age of six, I gave up what might have been a magnificent career as a painter. I had been disheartened by the failure of my Drawing Number One and my Drawing Number Two. Grown-ups never understand anything by themselves, and it is tiresome for children to be always and forever explaining things to them.');
  localStorage.setItem('story-1d', 'So then I chose another profession, and learned to pilot airplanes. I have flown a little over all parts of the world; and it is true that geography has been very useful to me. At a glance I can distinguish China from Arizona. If one gets lost in the night, such knowledge is valuable. In the course of this life I have had a great many encounters with a great many people who have been concerned with matters of consequence. I have lived a great deal among grown-ups. I have seen them intimately, close at hand. And that hasn\'t much improved my opinion of them. Whenever I met one of them who seemed to me at all clear-sighted, I tried the experiment of showing him my Drawing Number One, which I have always kept. I would try to find out, so, if this was a person of true understanding. But, whoever it was, he, or she, would always say: "That is a hat."Then I would never talk to that person about boa constrictors, or primeval forests, or stars. I would bring myself down to his level. I would talk to him about bridge, and golf, and politics, and neckties. And the grown-up would be greatly pleased to have met such a sensible man.');
- localStorage.setItem('story-1a-m', 'Suatu ketika ketika saya berumur enam tahun, saya melihat gambar yang indah dalam sebuah buku, yang disebut Kisah Benar dari Alam, tentang hutan purba. Ia adalah gambar seekor boa constrictor dalam tindakan menelan haiwan. Berikut adalah salinan lukisan tersebut. Dalam buku itu ia berkata: "Boa constrictors menelan mangsa mereka keseluruhan, tanpa mengunyahnya. Selepas itu mereka tidak dapat bergerak, dan mereka tidur melalui enam bulan yang mereka perlukan untuk penghadaman." Saya merenung sedalam-dalamnya, tentang pengembaraan hutan. Dan selepas beberapa kerja dengan pensel warna saya berjaya membuat lukisan pertama saya. Lukisan Saya Nombor Satu. Ia kelihatan seperti ini:');
- localStorage.setItem('story-1b-m', 'Saya menunjukkan karya agung saya kepada orang dewasa, dan bertanya kepada mereka sama ada lukisan itu menakutkan mereka. Tetapi mereka menjawab: "Takut? Mengapa ada orang yang takut dengan topi?" Lukisan saya bukan gambar topi. Ia adalah gambar seekor boa constrictor sedang mencerna seekor gajah. Tetapi kerana orang dewasa tidak dapat memahaminya, saya membuat lukisan lain: Saya melukis bahagian dalam boa constrictor, supaya orang dewasa dapat melihatnya dengan jelas. Mereka sentiasa perlu menjelaskan sesuatu. Lukisan Nombor Dua Saya kelihatan seperti ini:');
+ localStorage.setItem('story-1a-m', '<img src="images/story1-img2.png"> Suatu ketika ketika saya berumur enam tahun, saya melihat gambar yang indah dalam sebuah buku, yang disebut Kisah Benar dari Alam, tentang hutan purba. Ia adalah gambar seekor boa constrictor dalam tindakan menelan haiwan. Berikut adalah salinan lukisan tersebut. Dalam buku itu ia berkata: "Boa constrictors menelan mangsa mereka keseluruhan, tanpa mengunyahnya. Selepas itu mereka tidak dapat bergerak, dan mereka tidur melalui enam bulan yang mereka perlukan untuk penghadaman." Saya merenung sedalam-dalamnya, tentang pengembaraan hutan. Dan selepas beberapa kerja dengan pensel warna saya berjaya membuat lukisan pertama saya. Lukisan Saya Nombor Satu. Ia kelihatan seperti ini: <img src="images/story1-img3.png">');
+ localStorage.setItem('story-1b-m', 'Saya menunjukkan karya agung saya kepada orang dewasa, dan bertanya kepada mereka sama ada lukisan itu menakutkan mereka. Tetapi mereka menjawab: "Takut? Mengapa ada orang yang takut dengan topi?" Lukisan saya bukan gambar topi. Ia adalah gambar seekor boa constrictor sedang mencerna seekor gajah. Tetapi kerana orang dewasa tidak dapat memahaminya, saya membuat lukisan lain: Saya melukis bahagian dalam boa constrictor, supaya orang dewasa dapat melihatnya dengan jelas. Mereka sentiasa perlu menjelaskan sesuatu. Lukisan Nombor Dua Saya kelihatan seperti ini: <img src="images/story1-img4.png">');
  localStorage.setItem('story-1c-m', 'Tanggapan orang dewasa, kali ini, adalah menasihati saya supaya mengenepikan lukisan boa constrictors saya, sama ada dari dalam atau luar, dan sebaliknya menumpukan diri saya kepada geografi, sejarah, aritmetik dan tatabahasa. Itulah sebabnya, pada usia enam tahun, saya melepaskan apa yang mungkin merupakan kerjaya yang hebat sebagai pelukis. Saya telah kecewa dengan kegagalan Lukisan Nombor Satu dan Lukisan Nombor Dua saya. Orang dewasa tidak pernah memahami apa-apa dengan sendirinya, dan ia memenatkan untuk kanak-kanak sentiasa dan selama-lamanya menerangkan perkara kepada mereka.')
  localStorage.setItem('story-1d-m', 'Jadi saya memilih profesion lain, dan belajar memandu kapal terbang. Saya telah terbang sedikit ke seluruh dunia; dan memang benar bahawa geografi sangat berguna kepada saya. Sekali imbas saya boleh membezakan China dengan Arizona. Jika seseorang tersesat di malam hari, ilmu itu berharga. Dalam perjalanan hidup ini saya telah mengalami banyak perjumpaan dengan ramai orang yang mengambil berat tentang perkara akibat. Saya telah banyak hidup dalam kalangan orang dewasa. Saya telah melihat mereka secara dekat, dekat. Dan itu tidak banyak meningkatkan pendapat saya tentang mereka. Setiap kali saya bertemu dengan salah seorang daripada mereka yang pada pandangan saya kelihatan jelas, saya mencuba percubaan menunjukkan kepadanya Lukisan Nombor Satu saya, yang selalu saya simpan. Saya akan cuba mencari tahu, jadi, jika ini adalah orang yang benar-benar memahami. Tetapi, sesiapa sahaja, dia, atau dia, akan sentiasa berkata: "Itu adalah topi." Kemudian saya tidak akan bercakap dengan orang itu tentang boa constrictors, atau hutan purba, atau bintang. Saya akan membawa diri saya ke tahap dia. Saya akan bercakap dengannya tentang jambatan, dan golf, dan politik, dan tali leher. Dan orang dewasa akan sangat gembira dapat bertemu dengan lelaki yang bijak.')
  localStorage.setItem('story-2a', 'Once it was about one in the morning, the three of us headed back up to the room (our other three friends were already up there) and changed to go to sleep. Of course, we\'re all pretty young (no one above 21) so we logically stayed up awhile to talk. Our room was one of those annoying ones where you can\'t do anything to close the curtains, so they were wide open and some of us were just staring out the window while we talked. <br><br>After we\'d been talking awhile, one friend said, “Hey, what\'s that in the window over there?”');
@@ -49,6 +49,10 @@ var s3_p1a_mal = localStorage.getItem("story-3a-m");
 var s3_p1b_mal = localStorage.getItem("story-3b-m");
 var s3_p2a_mal = localStorage.getItem("story-3c-m");
 var s3_p2b_mal = localStorage.getItem("story-3d-m");
+var avg1;
+var avg2;
+var avg3;
+
 
 
 // Declare variables
@@ -74,10 +78,13 @@ var currentStoryMAL_2;
 
 var inputVoice = document.getElementById("voice-select");
 var inputLang = document.getElementById("lang-select");
+var rate = document.getElementById("storyRating"); //rating display
+
+
 
 function storyLoad() {
-  console.log("storyLoad is loaded!");
   console.log(pageNum);
+  console.log(avg1);
   switch (pageNum) {
     case '1': 
     currentStoryEN_1 = s1_p1a_en;
@@ -258,6 +265,16 @@ function resetVoice() {
   localStorage.removeItem('voice-select');
   localStorage.removeItem('lang-select');
   localStorage.removeItem('size');
+  localStorage.removeItem('count');
+  localStorage.removeItem('avg-s1');
+  localStorage.removeItem('avg-s2');
+  localStorage.removeItem('avg-s3');
+  localStorage.removeItem('star1');
+  localStorage.removeItem('star2');
+  localStorage.removeItem('star3');
+  localStorage.removeItem('star4');
+  localStorage.removeItem('star5');
+
 }
 
 function toSmall() {
@@ -283,3 +300,129 @@ function toLarge() {
   text1.style.fontSize = "2em";
   text2.style.fontSize = "2em";
 }
+
+  var ratez = document.getElementsByName("rating");
+  var storyAverage;
+  var story1Average;
+  var story2Average;
+  var story3Average;
+  var count = 0;
+  var star1 = 0;
+  var star2 = 0;
+  var star3 = 0;
+  var star4 = 0;
+  var star5 = 0;
+
+
+
+if(localStorage.getItem('count')){
+    count = localStorage.getItem('count');
+}else{
+    count = 0;
+     }
+
+     if(localStorage.getItem('star1')){
+
+   star1 = localStorage.getItem('star1');
+}else{
+    star1= 0;
+     }
+
+     if(localStorage.getItem('star2')){
+
+    star2 = localStorage.getItem('star2');
+}else{
+    star2 = 0;
+     }
+
+
+     if(localStorage.getItem('star3')){
+
+    star3 = localStorage.getItem('star3');
+}else{
+    star3 = 0;
+     }
+
+     if(localStorage.getItem('star4')){
+
+    star4 = localStorage.getItem('star4');
+}else{
+    star4 = 0;
+     }
+
+     if(localStorage.getItem('star5')){
+
+    star5 = localStorage.getItem('star5');
+}else{
+    star5 = 0;
+     }
+
+
+function confirm(){
+      
+  if (ratez[4].checked) {
+    count++;
+    localStorage.setItem('count', count)
+    star1++;
+    localStorage.setItem('star1', star1)
+    ratez[4].checked = "checked";
+  }
+  else if (ratez[3].checked) {
+    count++;
+    localStorage.setItem('count', count)
+    star2++;
+    localStorage.setItem('star2', star2)
+  
+  }
+  else if (ratez[2].checked) {
+    count++;
+    localStorage.setItem('count', count)
+    star3++;
+    localStorage.setItem('star3', star3)
+  }
+  else if (ratez[1].checked) {
+    count++;
+    localStorage.setItem('count', count)
+    star4++;
+    localStorage.setItem('star4', star4) 
+  }
+  else if (ratez[0].checked) {
+    count++;
+    localStorage.setItem('count', count)
+    star5++;
+    localStorage.setItem('star5', star5) 
+ }
+    storyAverage =  ((((5*parseInt(star5)) + (4*parseInt(star4)) + (3*parseInt(star3)) + (2*parseInt(star2)) + (parseInt(star1)))) / parseInt(count)).toFixed(1);
+
+    //alert(average);
+    for (var i = 0; i < ratez.length; i++) {
+           ratez[i].disabled = true;  
+    }
+
+    switch (pageNum) {
+      case '2':
+      story1Average = storyAverage;
+      localStorage.setItem('avg-s1', story1Average);
+      break;
+
+      case '4':
+      story2Average = storyAverage;
+      localStorage.setItem('avg-s2', story2Average);
+      break;
+
+      case '6':
+      story3Average = storyAverage;
+      localStorage.setItem('avg-s3', story3Average);
+      break;
+    }
+    
+    avg1 = localStorage.getItem('avg-s1');
+    console.log(avg1);
+    avg2 = localStorage.getItem('avg-s2');
+    console.log(avg2);
+    avg3 = localStorage.getItem('avg-s3');
+    console.log(avg3);
+
+  }
+
+    
